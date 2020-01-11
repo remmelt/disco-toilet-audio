ed5d63bf92f952234df59d6f5e0479305423ce6f
Remmelt Pit <remmelt@hipcode.io>
2020-01-11 16:33:34 +0100

    Nicer go, some additional changes

--- main.go
+++ main.go
@@ -16,13 +16,18 @@ import (
 
 var playing = false
 
-type BridgeIpResponse struct {
-	Id                string `json:"id"`
-	InternalIpAddress string `json:"internalipaddress"`
+// BridgeIPResponse is the response received from the Hue bridge API re: the IP of the bridge
+type BridgeIPResponse struct {
+	ID                string `json:"id"`
+	InternalIPAddress string `json:"internalipaddress"`
 }
+
+// SensorDataResponseState is the response received from the Hue bridge API re: the presence recorded by the sensor
 type SensorDataResponseState struct {
 	Presence bool `json:"presence"`
 }
+
+// SensorDataResponse is the response received from the Hue bridge API re: state of the sensor
 type SensorDataResponse struct {
 	State SensorDataResponseState `json:"state"`
 }
@@ -46,17 +51,17 @@ func getBridgeAddress() (string, error) {
 	if err != nil {
 		return "", err
 	}
-	var res []BridgeIpResponse
+	var res []BridgeIPResponse
 	json.Unmarshal([]byte(body), &res)
 	if len(res) != 1 {
 		return "", errors.New("Could not determine bridge IP, response length != 1")
 	}
 
-	return res[0].InternalIpAddress, nil
+	return res[0].InternalIPAddress, nil
 }
 
-func getSensorPresence(bridgeIpAddress string, username string) (bool, error) {
-	body, err := httpGet("http://" + bridgeIpAddress + "/api/" + username + "/sensors/4")
+func getSensorPresence(bridgeIPAddress string, username string) (bool, error) {
+	body, err := httpGet("http://" + bridgeIPAddress + "/api/" + username + "/sensors/4")
 	if err != nil {
 		return false, err
 	}
@@ -65,8 +70,8 @@ func getSensorPresence(bridgeIpAddress string, username string) (bool, error) {
 	return res.State.Presence, nil
 }
 
-func runMpcCmd(mpdIpAddress string, arg ...string) error {
-	arg = append([]string{"-h", "192.168.178.89"}, arg...)
+func runMpcCmd(mpdIPAddress string, arg ...string) error {
+	arg = append([]string{"-h", mpdIPAddress}, arg...)
 	log.Println(arg)
 	cmd := exec.Command("mpc", arg...)
 	err := cmd.Run()
@@ -76,12 +81,12 @@ func runMpcCmd(mpdIpAddress string, arg ...string) error {
 	return nil
 }
 
-func pause(mpdIpAddress string) error {
-	return runMpcCmd(mpdIpAddress, "pause")
+func pause(mpdIPAddress string) error {
+	return runMpcCmd(mpdIPAddress, "pause")
 }
 
-func play(mpdIpAddress string, arg ...string) error {
-	return runMpcCmd(mpdIpAddress, "play")
+func play(mpdIPAddress string, arg ...string) error {
+	return runMpcCmd(mpdIPAddress, "play")
 }
 
 func schedule(what func(), delay time.Duration) chan bool {
@@ -101,36 +106,41 @@ func schedule(what func(), delay time.Duration) chan bool {
 	return stop
 }
 
-func setPlayState(bridgeIpAddress string, username string, mpdIpAddress string, graceTime int) {
-	presence, err := getSensorPresence(bridgeIpAddress, username)
+func setPlayState(bridgeIPAddress string, username string, mpdIPAddress string, graceTime int) {
+	presence, err := getSensorPresence(bridgeIPAddress, username)
 	if err != nil {
 		log.Println(err)
+		return
 	}
 
 	if presence && !playing {
-		play(mpdIpAddress)
+		play(mpdIPAddress)
 		playing = true
-		time.Sleep(30 * time.Second)
+		time.Sleep(time.Duration(graceTime) * time.Second)
 	} else if !presence && playing {
-		pause(mpdIpAddress)
+		pause(mpdIPAddress)
 		playing = false
 	}
 }
 
-func initMpd(mpdIpAddress string, playlist string, volume string) error {
-	err := runMpcCmd(mpdIpAddress, "clear")
+func initMpd(mpdIPAddress string, playlist string, volume string) error {
+	err := runMpcCmd(mpdIPAddress, "clear")
 	if err != nil {
 		return errors.New("Could not clear playlist")
 	}
-	err = runMpcCmd(mpdIpAddress, "load", playlist)
+	err = runMpcCmd(mpdIPAddress, "load", playlist)
 	if err != nil {
-		return errors.New(fmt.Sprintf("Could not load playlist %s :(((", playlist))
+		return fmt.Errorf("Could not load playlist %s :(((", playlist)
 	}
-	err = runMpcCmd(mpdIpAddress, "repeat")
+	err = runMpcCmd(mpdIPAddress, "repeat")
 	if err != nil {
 		return errors.New("Could not set repeat")
 	}
-	err = runMpcCmd(mpdIpAddress, "volume", volume)
+	err = runMpcCmd(mpdIPAddress, "random")
+	if err != nil {
+		return errors.New("Could not set random")
+	}
+	err = runMpcCmd(mpdIPAddress, "volume", volume)
 	if err != nil {
 		return errors.New("Could not set volume")
 	}
@@ -154,29 +164,29 @@ func getEnvOrDie(key string) string {
 }
 
 func main() {
-	bridgeIpAddress := getEnvOrDie("HUE_BRIDGE_IP")
-	mpdIpAddress := getEnvOrDie("MPD_IP")
+	bridgeIPAddress := getEnvOrDie("HUE_BRIDGE_IP")
+	mpdIPAddress := getEnvOrDie("MPD_IP")
 	username := getEnvOrDie("HUE_USERNAME")
 	playlist := getEnvOrDie("PLAYLIST")
 	graceTime, err := strconv.Atoi(getEnv("SLEEP_AFTER_AWAY", "30"))
 	if err != nil {
 		log.Fatalln(fmt.Sprintf("This is not an int: %s, %v", getEnv("SLEEP_AFTER_AWAY", "30"), err))
 	}
-	volume := getEnv("VOLUME", "15")
+	volume := getEnv("VOLUME", "5")
 	if strconv.Atoi(volume); err != nil {
 		log.Fatalln(fmt.Sprintf("This is not an int: %s, %v", volume, err))
 	}
 
-	initMpd(mpdIpAddress, playlist, volume)
-	setPlayState(bridgeIpAddress, username, mpdIpAddress, graceTime)
+	initMpd(mpdIPAddress, playlist, volume)
+	setPlayState(bridgeIPAddress, username, mpdIPAddress, graceTime)
 
 	go func() {
 		sigchan := make(chan os.Signal, 10)
 		signal.Notify(sigchan, os.Interrupt)
 		<-sigchan
-		log.Println("Program killed !")
+		log.Println("Program killed!")
 
-		pause(mpdIpAddress)
+		pause(mpdIPAddress)
 
 		os.Exit(0)
 	}()
@@ -189,7 +199,7 @@ func main() {
 			case <-done:
 				return
 			case _ = <-ticker.C:
-				setPlayState(bridgeIpAddress, username, mpdIpAddress, graceTime)
+				setPlayState(bridgeIPAddress, username, mpdIPAddress, graceTime)
 			}
 		}
 	}()
