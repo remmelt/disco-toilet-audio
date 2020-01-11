82a546e5e52166db9e39f0daa2fd291f373b2d80
Remmelt Pit <remmelt@hipcode.io>
2020-01-11 16:33:54 +0100

    Script-like setup for the pi

--- pi-setup/install.sh
+++ pi-setup/install.sh
@@ -1,5 +1,7 @@
 #! /usr/bin/env bash
 
+# Does not quite work as a standalone unattended update script but you get the idea
+
 set -euo pipefail
 
 USER=remmelt
@@ -23,29 +25,38 @@ EOF
 
 adduser $USER
 usermod -a -G adm,dialout,cdrom,sudo,audio,video,plugdev,games,users,input,netdev,gpio,i2c,spi $USER
-cat $USER ALL=(ALL) PASSWD: ALL >> /etc/sudoers.d/010_pi-nopasswd
+echo "$USER ALL=(ALL) PASSWD: ALL" >> /etc/sudoers.d/010_pi-nopasswd
 
 pkill -u pi
 deluser -remove-home pi
 
-cat <<EOF >> /etc/ssh/sshd_config
+cat <<EOF > /etc/ssh/sshd_config
+X11Forwarding no
+PrintMotd no
+AcceptEnv LANG LC_*
+Subsystem sftp /usr/lib/openssh/sftp-server
 AllowUsers $USER
 ChallengeResponseAuthentication no
 PasswordAuthentication no
 UsePAM no
+PermitRootLogin no
 EOF
 
-mkdir ~$USER/.ssh/
-chmod 0700 ~$USER/.ssh/
-cat <<EOF > ~$USER/.ssh/authorized_keys
+mkdir /home/$USER/.ssh/
+chmod 0700 /home/$USER/.ssh/
+cat <<EOF > /home/$USER/.ssh/authorized_keys
 ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCrkq5rkorKGbqg0MclVexkF4OxFENSOqRR1S8PZV6I+hd3QlygIiv+mQmIBE96r7Ea9ar2VgHcnumLMsVpHgVqNAYpbsMCZqh0VXu8f3PHrv+DvbhnJJmhyejghxRPT9FWhWKj6VqcGs8U7i6EUHyimFHwl3t9R9DtyqH2hRf+GuNvLlo2WjptQlpHGTRSI3leXuWapKNKKKNmP8FSqK0EHojgwzgojeOD04qkvKpCaNuHzAQxlTvIXqChqzcQ2sffd64poZwOerxUYTyMcWR6bi24RSuVzN9UVRwnpK8D/WzD0wMMFhzeUcQmOdcbrHKs0ifJq+lGQEgKlqfdkIqbmHR0EsC0198fNjZUxuQoobvcBcESjPdNT6BCY6dMhIxrk9tugpOgii737NZMmSH/9zNt0QEsMpaEJeYkctclypQAveAnUytKie/dTfLNN8LEWUdlIbkO+0S+1nr9D6JFmBOvHiWxow25M40R3fElJkfvG1NQ5J/c76vrvw5OTndB24yhQfdoCkGdy8xjWCqCafpAYfPbD/IAzogk7MO9dlnHpQyIlG7FQhxRnJEMxDkLi841XnvuHGf0AvFwN0Ol3pTFAHR7Rc76RCWHShyG7CWRw0gjRietKtouxiCly4z91N0TUsBeBol8Yj8T71OyhRQ+rKd7LUCrt3Ul2ysidw== remmelt's mbp
 EOF
-chmod 0600 ~$USER/.ssh/authorized_keys
-chown -R $USER:$USER ~$USER
+chmod 0600 /home/$USER/.ssh/authorized_keys
+chown -R $USER:$USER /home/$USER
 
 git clone https://github.com/pimoroni/pirate-audio
 cd pirate-audio/mopidy
 bash install.sh
 apt-get install -y mpc
+touch /var/lib/mopidy/playlists/d.m3u
+chmod -R g+rwX /var/lib/mopidy/
+chmod -R g+s /var/lib/mopidy/
+mpc save Disco
 
 echo "We're done here. Reboot!"
